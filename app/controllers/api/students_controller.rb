ae94a99dfebcbb86a18ed81e0400df445905506f
funbrian <brianpdevane@gmail.com>
2020-05-04 21:03:39 -0400

    added authentication

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -1,2 +1,26 @@
 class Api::StudentsController < ApplicationController
+
+  def create
+    student = Student.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      personal_site_url: params[:personal_site_url],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      photo_url: params[:photo_url],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
+    )
+    if student.save
+      render json: { message: "Student created successfully" }, status: :created
+    else
+      render json: { errors: student.errors.full_messages }, status: :bad_request
+    end
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
+
+  def current_student
+    auth_headers = request.headers["Authorization"]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: "HS256" }
+        )
+        Student.find_by(id: decoded_token[0]["student_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  helper_method :current_student
+
+  def authenticate_student
+    unless current_student
+      render json: {}, status: :unauthorized
+    end
+  end
 end
