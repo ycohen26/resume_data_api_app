dfc64fc3686eb5bfc51b5e811ae31dc0823fd7a1
funbrian <brianpdevane@gmail.com>
2020-05-05 20:45:07 -0400

    added equal signs to show

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -26,17 +26,17 @@ class Api::StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
     if @student
-      @first_name: @student.first_name
-      @last_name: @student.last_name
-      @email: @student.email
-      @phone_number: @student.phone_number
-      @short_bio: @student.short_bio
-      @linkedin_url: @student.linkedin_url
-      @twitter_handle: @student.twitter_handle
-      @personal_site_url: @student.personal_site_url
-      @online_resume_url: @student.online_resume_url
-      @github_url: @student.github_url
-      @photo_url: @student.photo_url
+      @first_name = @student.first_name
+      @last_name = @student.last_name
+      @email = @student.email
+      @phone_number = @student.phone_number
+      @short_bio = @student.short_bio
+      @linkedin_url = @student.linkedin_url
+      @twitter_handle = @student.twitter_handle
+      @personal_site_url = @student.personal_site_url
+      @online_resume_url = @student.online_resume_url
+      @github_url = @student.github_url
+      @photo_url = @student.photo_url
       @experiences = @student.experiences
       @capstones = @student.capstones
       @skills = @student.skills
